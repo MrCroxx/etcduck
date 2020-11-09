65be21c68d2556e627dc76736af6efb12a2acdc0
MrCroxx <mrcroxx@outlook.com>
2020-11-09 21:56:06 +0800

    Add Client.ID() interface and impl.

--- client.go
+++ client.go
@@ -1,12 +1,15 @@
 package etcdance
 
 import (
+	"fmt"
+
 	"github.com/MrCroxx/etcdance/snowflake"
 	"go.etcd.io/etcd/clientv3"
 )
 
 // Client : etcdancer.Client
 type Client interface {
+	ID() int64
 }
 
 type client struct {
@@ -17,11 +20,19 @@ type client struct {
 
 // New : Create etcdance.Client from *etcd.clientv3.Client
 func New(cli *clientv3.Client, datacenterID int64, workerID int64) (Client, error) {
+	// check args
+	if cli == nil {
+		return nil, fmt.Errorf("etcd client cannot be nil")
+	}
+
+	// generate client id
 	snowflake.InitOrReset(datacenterID, workerID)
 	id, err := snowflake.NextID()
 	if err != nil || id == -1 {
 		return nil, err
 	}
+
+	// return client
 	return &client{
 		id:  id,
 		cli: cli,
@@ -30,5 +41,10 @@ func New(cli *clientv3.Client, datacenterID int64, workerID int64) (Client, erro
 }
 
 // Implement
+
+// ID : returns client id
+func (c *client) ID() int64 {
+	return c.id
+}
 
 // Utilities
